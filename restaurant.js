46b6f6cf5b615ec3d688ed0f5c45dd81168d3f4c
C4mz0r <aut0x@hotmail.com>
2016-07-20 17:31:59 -0600

    Add html content for each tab

--- restaurant.js
+++ restaurant.js
@@ -15,18 +15,48 @@ var drawTabs = function() {
 		
 	// The tabs
 	$('#tabs').append("<ul>" +
-					  "<li><a href='#tabs-1'>First</a></li>" +
-					  "<li><a href='#tabs-2'>Second</a></li>" +
+					  "<li><a href='#tabs-1'>Home</a></li>" +
+					  "<li><a href='#tabs-2'>Menu</a></li>" +
+					  "<li><a href='#tabs-3'>Contact</a></li>" +
 					  "</ul>");
 	
     // Content for first tab
 	$('#tabs').append(	"<div id='tabs-1'>" +
-						"<p>Here is content for first tab</p>" +
+						homePageHtml() +
 						"</div>");
 						
 	// Content for second tab
 	$('#tabs').append(	"<div id='tabs-2'>" +
-						"<p>Here is content for second tab</p>" +
+						menuPageHtml() +
+						"</div>");
+						
+	// Content for second tab
+	$('#tabs').append(	"<div id='tabs-3'>" +
+						contactPageHtml() +
 						"</div>");
 }
 
+var contactPageHtml = function() {
+	var contact = "<h1>The Chocolate Truffle</h1>";
+	contact += "<p>Located in the heart of the Alpine Valley in Somewhere, Nowhereland.</p>";
+	contact += "<p>Open weekdays 11 AM until 9 PM, weekends 11 AM until 11 PM";
+	contact += "<p>Phone: 555-5555</p>";
+	return contact;
+}
+
+var homePageHtml = function() {
+	var home = "<h1>The Chocolate Truffle</h1>";
+	home += "The best fine dining in your town!";
+	return home;
+}
+
+var menuPageHtml = function() {
+	var menu = "<h1>The Chocolate Truffle</h1>";
+	menu += "<table>";
+	menu += "<tr><td>Chocolate Truffles</td><td>$12.95</td></tr>";
+	menu += "<tr><td>Cheesecake</td><td>$8.95</td></tr>";
+	menu += "<tr><td>Tenderloin</td><td>$39.95</td></tr>";
+	menu += "<tr><td>Lobster Tail</td><td>$49.95</td></tr>";
+	menu += "</table>";
+	return menu;
+}
